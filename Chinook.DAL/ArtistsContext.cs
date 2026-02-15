96aa4766f3164d9a4c407eb013d5fc6d0694393d
karol-kot2007 <karol.kot.2007@gmail.com>
2026-02-15 18:30:46 +0100

    dodany folder DAL

---
+++ Chinook.DAL/ArtistsContext.cs
@@ -0,0 +1,42 @@
+﻿using Chinook.DAL.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Chinook.DAL
+{
+  public class ArtistContext : DbContext
+  {
+    public DbSet<Track> Tracks { get; set; }
+    public DbSet<Artist> Artists { get; set; }
+    public DbSet<Album> Albums { get; set; }
+
+
+
+
+    public DbSet<AlbumTracks> AlbumTracks { get; set; }
+
+    // tabvele jak powzyej zrobvic 
+    public static string chinookPath = "chinook.db";
+    public string DbPath { get; }
+    public ArtistContext()
+    {
+      Console.WriteLine($"Database path: {chinookPath}.");
+      DbPath = chinookPath;
+    }
+    public List<Artist> GetArtists()
+    {
+      return Artists.ToList();
+    }
+    protected override void OnConfiguring(DbContextOptionsBuilder options)
+    {
+      options.UseSqlite($"Data Source={chinookPath}");
+    }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+      modelBuilder.Entity<Track>().HasKey(x => x.TrackId);
+      modelBuilder.Entity<Artist>().HasKey(x => x.ArtistId);
+      
+      modelBuilder.Entity<AlbumTrack>().HasNoKey();
+      modelBuilder.Entity<AlbumTracks>().HasNoKey();
+    }
+  }
+}
---
+++ Chinook.DAL/Models.cs
@@ -0,0 +1,50 @@
+﻿
+
+namespace Chinook.DAL.Models
+{
+
+  public class AlbumTrack
+  {
+    public int artistId { get; set; }
+    public string? artistName { get; set; }
+    public int albumId { get; set; }
+    public int trackId { get; set; }
+    public string? albumName { get; set; }
+    public string? trackName { get; set; }
+  }
+  public class Track
+  {
+    public int GenreId { get; set; }
+    public int MediaTypeId { get; set; }
+    public int AlbumId { get; set; }
+    public int TrackId { get; set; }
+    public string? Name { get; set; }
+    public string? Composer { get; set; }
+    public int Milliseconds { get; set; }
+    public int Bytes { get; set; }
+    public string? LocalPath { get; set; }
+    public float UnitPrice { get; set; }
+
+  }
+  public class Artist
+  {
+    public string? Name { get; set; }
+    public int ArtistId { get; set; }
+    //public int albumId { get; set; }
+  }
+  public class Album
+  {
+    public string? Title { get; set; }
+    public int AlbumId { get; set; }
+    public int ArtistId { get; set; }
+  }
+  public class AlbumTracks
+  {
+    public string? artistName { get; set; }
+    public string? albumName { get; set; }
+    public int artistId { get; set; }
+    public int albumId { get; set; }
+    public int trackId { get; set; }
+    public string? trackName { get; set; }
+  }
+}
