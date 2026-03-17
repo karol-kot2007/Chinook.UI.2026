3322a578cbfbe6be36e58158e9a7f5abf3070cdc
karol-kot2007 <karol.kot.2007@gmail.com>
2026-03-17 17:39:58 +0100

    Blazor poprawiony

--- Chinook.Blazer/Program.cs
+++ Chinook.Blazer/Program.cs
@@ -4,30 +4,21 @@ using Chinook.Models;
 using SQLitePCL;
 
 var builder = WebApplication.CreateBuilder(args);
-
 Batteries_V2.Init();
-// Add services to the container.
-// W sekcji builder.Services
 builder.Services.AddDbContext<ArtistContext>();
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
-
+builder.Services.AddScoped<IRepository, Repository>();
 var app = builder.Build();
-builder.Services.AddScoped<IRepository>();
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
   app.UseExceptionHandler("/Error", createScopeForErrors: true);
-  // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+ 
   app.UseHsts();
 }
-
 app.UseHttpsRedirection();
-
 app.UseStaticFiles();
 app.UseAntiforgery();
-
 app.MapRazorComponents<App>()
     .AddInteractiveServerRenderMode();
-
 app.Run();
