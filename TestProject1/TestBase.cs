22a1600d77f7ade9b55abcbf082e27ca789719f2
karol-kot2007 <karol.kot.2007@gmail.com>
2026-03-29 21:07:40 +0200

    indeksowanie od 0

--- TestProject1/TestBase.cs
+++ TestProject1/TestBase.cs
@@ -32,75 +32,71 @@ namespace Chinook.Tests
       Assert.IsNotNull(albumInfo.AlbumInfo.AlbumInfo.Max);
       return albumInfo;
     }
-
-    protected void DoLoadNextArtist(IRepository repo)
+    protected void DoIndexOperation(IRepository repo, Repository.Operation op)
     {
       var albumInfo = DoLoadFirstArtist(repo);
-
       var albumInfo1 = repo.BuildModel(null);
-      var albumInfo2 = repo.BuildModel(albumInfo1, Repository.Operation.NextArtist);
-      var albumInfo3 = repo.BuildModel(albumInfo1, Repository.Operation.NextArtist);
-
+      var albumInfo2 = repo.BuildModel(albumInfo1, op);
+      var albumInfo3 = repo.BuildModel(albumInfo1, op);
       Assert.IsNotNull(albumInfo1);
       Assert.IsNotNull(albumInfo2);
       Assert.IsNotNull(albumInfo3);
+      switch (op)
+      {
+        case Repository.Operation.NextArtist:
+          Assert.AreEqual(albumInfo1.CurrentArtistIndex, 0);
+          Assert.AreEqual(albumInfo2.CurrentArtistIndex, 1);
+          Assert.AreEqual(albumInfo3.CurrentArtistIndex, 2);
+          Assert.AreNotEqual(albumInfo1.CurrentArtistIndex, albumInfo2.CurrentArtistIndex);
+          Assert.AreNotEqual(albumInfo2.CurrentArtistIndex, albumInfo3.CurrentArtistIndex);
+          Assert.AreEqual(albumInfo1.CurrentAlbumIndex, albumInfo3.CurrentAlbumIndex);
+          break;
+        case Repository.Operation.PrevArtist:
+          Assert.AreEqual(albumInfo1.CurrentArtistIndex, 0);
+          //Assert.AreEqual(albumInfo2.CurrentArtistIndex, albumInfo2.MaxArtistIndex);
+          //Assert.AreEqual(albumInfo3.CurrentArtistIndex, albumInfo3.MaxArtistIndex-1);
+          Assert.AreNotEqual(albumInfo1.CurrentArtistIndex, albumInfo2.CurrentArtistIndex);
+          Assert.AreNotEqual(albumInfo2.CurrentArtistIndex, albumInfo3.CurrentArtistIndex);
+          Assert.AreNotEqual(albumInfo1.CurrentArtistIndex, albumInfo3.CurrentArtistIndex);
+          break;
+        case Repository.Operation.NextAlbum:
+          //1 artysta ma 2 albumy
+          Assert.AreEqual(albumInfo1.CurrentAlbumIndex, 0);
+          Assert.AreEqual(albumInfo2.CurrentAlbumIndex, 1);
+          Assert.AreEqual(albumInfo3.CurrentAlbumIndex, 0);
+          Assert.AreNotEqual(albumInfo1.CurrentAlbumIndex, albumInfo2.CurrentAlbumIndex);
+          Assert.AreNotEqual(albumInfo2.CurrentAlbumIndex, albumInfo3.CurrentAlbumIndex);
+          Assert.AreEqual(albumInfo1.CurrentAlbumIndex, albumInfo3.CurrentAlbumIndex);
+          break;
+        case Repository.Operation.PrevAlbum:
+          Assert.AreEqual(albumInfo1.CurrentAlbumIndex, 0);
+          Assert.AreEqual(albumInfo2.CurrentAlbumIndex, 1);
+          Assert.AreEqual(albumInfo3.CurrentAlbumIndex, 0);
+          Assert.AreNotEqual(albumInfo1.CurrentAlbumIndex, albumInfo2.CurrentAlbumIndex);
+          Assert.AreNotEqual(albumInfo2.CurrentAlbumIndex, albumInfo3.CurrentAlbumIndex);
+          Assert.AreEqual(albumInfo1.CurrentAlbumIndex, albumInfo3.CurrentAlbumIndex);
+          break;
+      }
 
-      Assert.AreNotEqual(albumInfo1.CurrentArtistIndex, albumInfo2.CurrentArtistIndex);
-      Assert.AreNotEqual(albumInfo2.CurrentArtistIndex, albumInfo3.CurrentArtistIndex);
-      Assert.AreNotEqual(albumInfo1.CurrentArtistIndex, albumInfo3.CurrentArtistIndex);
     }
-
+    protected void DoLoadNextArtist(IRepository repo)
+    {
+      DoIndexOperation(repo, Repository.Operation.NextArtist);
+    }
 
     protected void DoLoadPrevArtist(IRepository repo)
     {
-      var albumInfo = DoLoadFirstArtist(repo);
-
-      var albumInfo1 = repo.BuildModel(null);
-      var albumInfo2 = repo.BuildModel(albumInfo1, Repository.Operation.PrevArtist);
-      var albumInfo3 = repo.BuildModel(albumInfo1, Repository.Operation.PrevArtist);
-
-      Assert.IsNotNull(albumInfo1);
-      Assert.IsNotNull(albumInfo2);
-      Assert.IsNotNull(albumInfo3);
-
-      Assert.AreNotEqual(albumInfo1.CurrentArtistIndex, albumInfo2.CurrentArtistIndex);
-      Assert.AreNotEqual(albumInfo2.CurrentArtistIndex, albumInfo3.CurrentArtistIndex);
-      Assert.AreNotEqual(albumInfo1.CurrentArtistIndex, albumInfo3.CurrentArtistIndex);
+      DoIndexOperation(repo, Repository.Operation.PrevArtist);
     }
-
 
     protected void DoLoadNextAlbum(IRepository repo)
     {
-      var albumInfo = DoLoadFirstArtist(repo);
-
-      var albumInfo1 = repo.BuildModel(null);
-      var albumInfo2 = repo.BuildModel(albumInfo1, Repository.Operation.NextAlbum);
-      var albumInfo3 = repo.BuildModel(albumInfo1, Repository.Operation.NextAlbum);
-
-      Assert.IsNotNull(albumInfo1);
-      Assert.IsNotNull(albumInfo2);
-      Assert.IsNotNull(albumInfo3);
-
-      Assert.AreNotEqual(albumInfo1.CurrentAlbumIndex, albumInfo2.CurrentAlbumIndex);
-      Assert.AreNotEqual(albumInfo2.CurrentAlbumIndex, albumInfo3.CurrentAlbumIndex);
-      Assert.AreEqual(albumInfo1.CurrentAlbumIndex, albumInfo3.CurrentAlbumIndex);
+      DoIndexOperation(repo, Repository.Operation.NextAlbum);
     }
 
     protected void DoLoadPrevAlbum(IRepository repo)
     {
-      var albumInfo = DoLoadFirstArtist(repo);
-
-      var albumInfo1 = repo.BuildModel(null);
-      var albumInfo2 = repo.BuildModel(albumInfo1, Repository.Operation.PrevAlbum);
-      var albumInfo3 = repo.BuildModel(albumInfo1, Repository.Operation.PrevAlbum);
-
-      Assert.IsNotNull(albumInfo1);
-      Assert.IsNotNull(albumInfo2);
-      Assert.IsNotNull(albumInfo3);
-
-      Assert.AreNotEqual(albumInfo1.CurrentAlbumIndex, albumInfo2.CurrentAlbumIndex);
-      Assert.AreNotEqual(albumInfo2.CurrentAlbumIndex, albumInfo3.CurrentAlbumIndex);
-      Assert.AreEqual(albumInfo1.CurrentAlbumIndex, albumInfo3.CurrentAlbumIndex);
+      DoIndexOperation(repo, Repository.Operation.PrevAlbum);
     }
   }
 }
